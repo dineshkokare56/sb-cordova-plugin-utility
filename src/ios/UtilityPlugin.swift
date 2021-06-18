91c3e55cbcfcdb4350e6fb88d599fee05141824e
dineshkokare56 <81946841+dineshkokare56@users.noreply.github.com>
2021-06-18 12:58:06 +0530

    Added it.

---
+++ src/ios/UtilityPlugin.swift
@@ -0,0 +1,134 @@
+
+import Foundation
+
+
+@objc(UtilityPlugin) class UtilityPlugin : CDVPlugin { 
+
+
+    @objc
+    func openGooglePlay(_ command: CDVInvokedUrlCommand) {
+    
+    }
+    @objc
+    func getDeviceAPILevel(_ command: CDVInvokedUrlCommand) {
+
+    }
+
+    @objc
+    func checkAppAvailability(_ command: CDVInvokedUrlCommand) {
+        
+    }
+
+    @objc
+    func getDownloadDirectoryPath(_ command: CDVInvokedUrlCommand) {
+
+    }
+
+    @objc
+    func getIdOfResource(_ command: CDVInvokedUrlCommand) {
+
+    }
+
+    @objc 
+    func exportApk(_ command: CDVInvokedUrlCommand) {
+
+    }
+
+    @objc
+    func getApkSize(_ command: CDVInvokedUrlCommand) {
+    
+    }
+
+    @objc
+    func createDirectories(_ command: CDVInvokedUrlCommand) {
+
+    }
+
+    @objc
+    func writeFile(_ command: CDVInvokedUrlCommand) {
+
+    }
+
+    @objc
+    func getAvailableInternalMemorySize(_ command: CDVInvokedUrlCommand) {
+
+    }
+
+    @objc
+    func getUtmInfo(_ command: CDVInvokedUrlCommand) {
+
+    }
+
+
+
+    @objc
+    func clearUtmInfo(_ command: CDVInvokedUrlCommand) {
+
+    }
+
+
+@objc
+    func removeDirectory(_ command: CDVInvokedUrlCommand) {
+
+    }
+       @objc
+       func copyDirectory(_ command: CDVInvokedUrlCommand) {
+
+
+       }
+
+    @objc
+       func getStorageVolumes(_ command: CDVInvokedUrlCommand) {
+
+           
+       }
+
+       @objc
+       func renameDirectory(_ command: CDVInvokedUrlCommand) {
+
+           
+       }
+
+       @objc
+       func canWrite(_ command: CDVInvokedUrlCommand) {
+
+           
+       }
+
+
+       @objc
+       func getUsableSpace(_ command: CDVInvokedUrlCommand) {
+
+           
+       }
+
+
+       @objc
+       func readFromAssets(_ command: CDVInvokedUrlCommand) {
+
+           
+       }
+
+
+       @objc
+       func copyFile(_ command: CDVInvokedUrlCommand) {
+
+           
+       }
+
+
+       @objc
+       func verifyCaptcha(_ command: CDVInvokedUrlCommand) {
+
+           
+       }
+
+       @objc
+       func verify(_ command: CDVInvokedUrlCommand) {
+
+           
+       }
+
+       
+
+}
